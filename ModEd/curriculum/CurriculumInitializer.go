b9ef65b9c7222891bd81ecf9c2278147ab3d19b6
wavekanit <wavekanit@hotmail.com>
2025-08-19 15:31:39 +0700

    feat: add controller into initialize in module curriculum

--- ModEd/curriculum/CurriculumInitializer.go
+++ ModEd/curriculum/CurriculumInitializer.go
@@ -1,7 +1,14 @@
 package curriculum
 
-import "ModEd/core"
+import (
+	"ModEd/core"
+	"ModEd/curriculum/controller"
+)
 
 func InitialCurriculum() {
 	application := core.GetApplication()
+	application.AddController(controller.NewClassController())
+	application.AddController(controller.NewClassMaterialController())
+	application.AddController(controller.NewCoursePlanController())
+	application.AddController(controller.NewCurriculumController())
 }

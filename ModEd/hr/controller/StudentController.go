0985825b6a3f96bdb7555a523f859b935fc550ce
Mawin Srichat <120245459+WinRafaelx@users.noreply.github.com>
2025-08-19 15:14:46 +0700

    feat: hr student controller

--- ModEd/hr/controller/StudentController.go
+++ ModEd/hr/controller/StudentController.go
@@ -2,10 +2,26 @@ package controller
 
 import (
 	"ModEd/core"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 type StudentController struct {
 	application *core.ModEdApplication
+}
+
+func (controller *StudentController) RenderMain(context *fiber.Ctx) error {
+	return context.SendString("Hello World")
+}
+
+func (controller *StudentController) GetRoute() []*core.RouteItem {
+	routeList := []*core.RouteItem{}
+	routeList = append(routeList, &core.RouteItem{
+		Route:   "/hr/Student",
+		Handler: controller.RenderMain,
+		Method:  core.GET,
+	})
+	return routeList
 }
 
 func (controller *StudentController) SetApplication(application *core.ModEdApplication) {

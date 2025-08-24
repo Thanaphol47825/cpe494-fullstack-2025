81975e97ee4d9e7dd278a330a7fb19a99de421f9
Thanaphol(WD MBP) <thanaphol.iqtha@gmail.com>
2025-08-24 21:28:15 +0700

    feat: migration strategy
    Co-authored-by: chaiyapatoam <chaiyapatoam12@gmail.com>

---
+++ ModEd/core/migration/MigrationManager.go
@@ -0,0 +1,103 @@
+package migration
+
+import (
+	"ModEd/utils/deserializer"
+	"fmt"
+
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type MigrationManager struct {
+	DB                   *gorm.DB
+	models               []interface{}
+	seedDatas            map[string]interface{}
+	migrationStrategyMap map[ModuleOptionEnum]MigrationStrategy
+}
+
+func NewMigrationManager(DB *gorm.DB) *MigrationManager {
+	migrationMap := make(map[ModuleOptionEnum]MigrationStrategy)
+
+	// To use the core migration module you need to create your own migration strategy
+	// Then come here to replace `nil` with your model here to register
+	migrationMap[MODULE_COMMON] = &CommonMigrationStrategy{}
+	migrationMap[MODULE_RECRUIT] = &RecuitMigrationStrategy{}
+
+	seedDataMap := make(map[string]interface{})
+	models := []interface{}{}
+	// Migrate all modules at once
+	for module := range migrationMap {
+		if migrationMap[module] != nil {
+			models = append(models, migrationMap[module].GetModels()...)
+		}
+	}
+
+	return &MigrationManager{
+		DB:                   DB,
+		migrationStrategyMap: migrationMap,
+		models:               models,
+		seedDatas:            seedDataMap,
+	}
+}
+
+func (m *MigrationManager) MigrateToDB() error {
+	var modelsToMigrate []interface{}
+	for i := range m.models {
+		if m.DB.Migrator().HasTable(m.models[i]) {
+			continue
+		}
+
+		modelsToMigrate = append(modelsToMigrate, m.models[i])
+	}
+
+	if err := m.DB.AutoMigrate(modelsToMigrate...); err != nil {
+		return fmt.Errorf("failed to migrate to db: %w", err)
+	}
+	return nil
+}
+
+func (m *MigrationManager) DropAllTables() error {
+
+	if m.DB == nil {
+		return errors.New("db not initialize")
+	}
+
+	err := m.DB.Migrator().DropTable(m.models...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *MigrationManager) LoadSeedData() error {
+	for path, md := range m.seedDatas {
+		fmt.Println("Loading seed data from path:", path)
+
+		fd, err := deserializer.NewFileDeserializer(path)
+		if err != nil {
+			fmt.Printf("Error creating deserializer for %s: %v\n", path, err)
+			return err
+		}
+
+		fmt.Printf("FileDeserializer: %+v\n", fd)
+
+		err = fd.Deserialize(md)
+		if err != nil {
+			fmt.Printf("Error deserializing %s: %v\n", path, err)
+			return err
+		}
+
+		fmt.Printf("Deserialized data for %s: %+v\n", path, md)
+
+		result := m.DB.Create(md)
+		if result.Error != nil {
+			fmt.Printf("Error creating records from %s: %v\n", path, result.Error)
+			return result.Error
+		}
+
+		fmt.Printf("Successfully loaded data from %s: %d rows affected\n", path, result.RowsAffected)
+	}
+
+	return nil
+}
---
+++ ModEd/core/migration/RecuitMigrationStrategy.go
@@ -0,0 +1,21 @@
+package migration
+
+import "ModEd/recruit/model"
+
+type RecuitMigrationStrategy struct {
+}
+
+func (s *RecuitMigrationStrategy) GetModels() []interface{} {
+	return []interface{}{
+		&model.Admin{},
+		&model.Applicant{},
+		&model.ApplicationRound{},
+		&model.InterviewCriteria{},
+		&model.Interview{},
+		&model.ApplicationReport{},
+	}
+}
+
+func (s *RecuitMigrationStrategy) GetSeedPath() []interface{} {
+	return nil
+}

4fa436a1e18528a23cc353b0336382d40690269a
Jarukit-PM <114938171+Jarukit-PM@users.noreply.github.com>
2025-08-25 14:49:49 +0700

    feat: add Instructor_controller

--- ModEd/hr/HrInitializer.go
+++ ModEd/hr/HrInitializer.go
@@ -8,5 +8,6 @@ import (
 func InitialCommon() {
 	application := core.GetApplication()
 	application.AddController(controller.NewStudentController())
+	application.AddController(controller.NewInstructorController())
 	// application.AddController(controller.NewDepartmentController())
 }

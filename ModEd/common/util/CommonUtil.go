5877e4c4d07d4bfb7671bd70e88bd89fb2e239f6
CirrusPreeminent <153276550+CirrusPreeminent@users.noreply.github.com>
2025-08-26 10:16:50 +0700

    json import util

---
+++ ModEd/common/util/CommonUtil.go
@@ -0,0 +1,9 @@
+package util
+
+// func CommonDeserializer(path string) deserializer.FileDeserializer {
+// 	deserializer, err := deserializer.NewFileDeserializer(path)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 	return *deserializer
+// }

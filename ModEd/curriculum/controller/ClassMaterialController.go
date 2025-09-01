a231d2f1ab1fd8436dd48aebd21b81d8ea9df409
wavekanit <wavekanit@hotmail.com>
2025-09-01 17:49:15 +0700

    feat: add CRUD in classMaterial Controller and Hander in curriculum module

--- ModEd/curriculum/controller/ClassMaterialController.go
+++ ModEd/curriculum/controller/ClassMaterialController.go
@@ -27,13 +27,39 @@ func (controller *ClassMaterialController) GetRoute() []*core.RouteItem {
 	})
 
 	routeList = append(routeList, &core.RouteItem{
+		Route:  "/curriculum/ClassMaterial/createClassMaterial",
+		Handler: controller.handler.CreateClassMaterial,
+		Method:  core.POST,
+	})
+
+	routeList = append(routeList, &core.RouteItem{
+		Route:  "/curriculum/ClassMaterial/getClassMaterial/:id",
+		Handler: controller.handler.GetClassMaterialById,
+		Method:  core.GET,
+	})
+
+	routeList = append(routeList, &core.RouteItem{
 		Route:   "/curriculum/ClassMaterial/getClassMaterials",
 		Handler: controller.handler.GetClassMaterials,
 		Method:  core.GET,
 	})
+
+	routeList = append(routeList, &core.RouteItem{
+		Route:  "/curriculum/ClassMaterial/updateClassMaterial/",
+		Handler: controller.handler.UpdateClassMaterial,
+		Method:  core.POST,
+	})
+
+	routeList = append(routeList, &core.RouteItem{
+		Route:  "/curriculum/ClassMaterial/deleteClassMaterial/:id",
+		Handler: controller.handler.DeleteClassMaterial,
+		Method:  core.GET,
+	})
+
 	return routeList
 }
 
 func (controller *ClassMaterialController) SetApplication(application *core.ModEdApplication) {
 	controller.application = application
+	controller.handler.DB = application.DB
 }
--- ModEd/curriculum/controller/handler/ClassMaterialHandler.go
+++ ModEd/curriculum/controller/handler/ClassMaterialHandler.go
@@ -2,12 +2,14 @@ package handler
 
 import (
 	"ModEd/curriculum/model"
-	"ModEd/curriculum/utils"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
-type ClassMaterialHandler struct{}
+type ClassMaterialHandler struct {
+	DB *gorm.DB
+}
 
 func NewClassMaterialHandler() *ClassMaterialHandler {
 	return &ClassMaterialHandler{}
@@ -17,20 +19,100 @@ func (h *ClassMaterialHandler) RenderMain(context *fiber.Ctx) error {
 	return context.SendString("Class Material Main Page")
 }
 
-func (h *ClassMaterialHandler) GetClassMaterials(context *fiber.Ctx) error {
-	filePath := "/workspace/ModEd/curriculum/data/curriculum/classMaterial.json"
+func (h *ClassMaterialHandler) CreateClassMaterial(context *fiber.Ctx) error {
+	var payload *model.ClassMaterial
+	if err := context.BodyParser(&payload); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
 
-	classMaterialsMapper, err := utils.CreateMapper[model.ClassMaterial](filePath)
+	if err := h.DB.Create(payload).Error; err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    err.Error(),
+		})
+	}
+
+	return context.JSON(fiber.Map{
+		"isSuccess": true,
+		"result":    "Class material created successfully",
+	})
+}
+
+func (h *ClassMaterialHandler) GetClassMaterialById(context *fiber.Ctx) error {
+	id, err := context.ParamsInt("id")
 	if err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    "invalid id",
+		})
+	}
+
+	var classMaterial model.ClassMaterial
+	if err := h.DB.First(&classMaterial, id).Error; err != nil {
 		return context.JSON(fiber.Map{
 			"isSuccess": false,
 			"result":    "failed to get curriculums",
 		})
 	}
 
-	classMaterials := classMaterialsMapper.Deserialize()
+	return context.JSON(fiber.Map{
+		"isSuccess": true,
+		"result":    classMaterial,
+	})
+}
+
+func (h *ClassMaterialHandler) GetClassMaterials(context *fiber.Ctx) error {
+	var classMaterials []model.ClassMaterial
+	if err := h.DB.Find(&classMaterials).Error; err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    "failed to get class materials",
+		})
+	}
+
 	return context.JSON(fiber.Map{
 		"isSuccess": true,
 		"result":    classMaterials,
+	})
+}
+
+func (h *ClassMaterialHandler) UpdateClassMaterial(context *fiber.Ctx) error {
+	var payload *model.ClassMaterial
+	if err := context.BodyParser(&payload); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	if err := h.DB.Save(payload).Error; err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    "failed to update class material",
+		})
+	}
+
+	return context.JSON(fiber.Map{
+		"isSuccess": true,
+		"result":    "Class material updated successfully",
+	})
+}
+
+func (h *ClassMaterialHandler) DeleteClassMaterial(context *fiber.Ctx) error {
+	id, err := context.ParamsInt("id")
+	if err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    "invalid id",
+		})
+	}
+
+	if err := h.DB.Delete(&model.ClassMaterial{}, id).Error; err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    "failed to delete class material",
+		})
+	}
+
+	return context.JSON(fiber.Map{
+		"isSuccess": true,
+		"result":    "Class material deleted successfully",
 	})
 }

e8c935b5aac7355e0e403a7df215d98b7efa373c
Thichanon Sittisomboon <thichanonsittisomboon@gmail.com>
2025-09-01 21:59:42 +0700

    Update Recruit Initializer

--- ModEd/recruit/RecruitInitializer.go
+++ ModEd/recruit/RecruitInitializer.go
@@ -9,6 +9,8 @@ func InitialRecruit() {
 	application := core.GetApplication()
 	application.AddController(controller.NewAdminController())
 	application.AddController(controller.NewApplicantController())
+	application.AddController(controller.NewApplicationReportController())
+	application.AddController(controller.NewApplicationRoundController())
 	application.AddController(controller.NewApplicationStatusController())
 	application.AddController(controller.NewInterviewController())
 	application.AddController(controller.NewInterviewCriteriaController())

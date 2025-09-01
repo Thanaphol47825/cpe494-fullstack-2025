32cd41702689a77cf3b231ea87cb4888adfc7e9e
manatsanan0209 <manatsanan.ninrueang@gmail.com>
2025-09-01 21:44:33 +0700

    feat: implement CRUD in class controller and handler within curriculum module

--- ModEd/curriculum/controller/ClassController.go
+++ ModEd/curriculum/controller/ClassController.go
@@ -23,14 +23,41 @@ func (controller *ClassController) GetRoute() []*core.RouteItem {
 		Handler: controller.handler.RenderMain,
 		Method:  core.GET,
 	})
+
+	routeList = append(routeList, &core.RouteItem{
+		Route:   "/curriculum/Class/createClass",
+		Handler: controller.handler.CreateClass,
+		Method:  core.POST,
+	})
+
+	routeList = append(routeList, &core.RouteItem{
+		Route:   "/curriculum/Class/getClass/:id",
+		Handler: controller.handler.GetClassById,
+		Method:  core.GET,
+	})
+
 	routeList = append(routeList, &core.RouteItem{
 		Route:   "/curriculum/Class/getClasses",
 		Handler: controller.handler.GetClasses,
 		Method:  core.GET,
 	})
+
+	routeList = append(routeList, &core.RouteItem{
+		Route:   "/curriculum/Class/updateClass",
+		Handler: controller.handler.UpdateClass,
+		Method:  core.POST,
+	})
+
+	routeList = append(routeList, &core.RouteItem{
+		Route:   "/curriculum/Class/deleteClass/:id",
+		Handler: controller.handler.DeleteClass,
+		Method:  core.GET,
+	})
+
 	return routeList
 }
 
 func (controller *ClassController) SetApplication(application *core.ModEdApplication) {
 	controller.application = application
+	controller.handler.DB = application.DB
 }

385bee6d94cb7a31cd20db12275ae62a7c768738
satanakorn <satanakorn6.codercamp@gmail.com>
2025-09-01 18:15:46 +0700

    Interview CRUD and Mockup Data

--- ModEd/recruit/controller/InterviewController.go
+++ ModEd/recruit/controller/InterviewController.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"ModEd/core"
+	"ModEd/recruit/model"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -15,21 +16,172 @@ func NewInterviewController() *InterviewController {
 	return controller
 }
 
-func (controller *InterviewController) RenderMain(context *fiber.Ctx) error {
-	return context.SendString("hello common/Interview")
+func (controller *InterviewController) SetApplication(application *core.ModEdApplication) {
+	controller.application = application
 }
 
 func (controller *InterviewController) GetRoute() []*core.RouteItem {
 	routeList := []*core.RouteItem{}
+
 	routeList = append(routeList, &core.RouteItem{
-		Route:   "/recruit/InterviewController",
-		Handler: controller.RenderMain,
+		Route:   "/recruit/CreateInterview",
+		Handler: controller.CreateInterview,
+		Method:  core.POST,
+	})
+
+	routeList = append(routeList, &core.RouteItem{
+		Route:   "/recruit/GetInterviews",
+		Handler: controller.GetAllInterviews,
 		Method:  core.GET,
+	})
+
+	routeList = append(routeList, &core.RouteItem{
+		Route:   "/recruit/GetInterview/:id",
+		Handler: controller.GetInterviewByID,
+		Method:  core.GET,
+	})
+
+	routeList = append(routeList, &core.RouteItem{
+		Route:   "/recruit/UpdateInterview/:id",
+		Handler: controller.UpdateInterview,
+		Method:  core.POST,
+	})
+
+	routeList = append(routeList, &core.RouteItem{
+		Route:   "/recruit/DeleteInterview/:id",
+		Handler: controller.DeleteInterview,
+		Method:  core.POST,
 	})
 
 	return routeList
 }
 
-func (controller *InterviewController) SetApplication(application *core.ModEdApplication) {
-	controller.application = application
+func (controller *InterviewController) CreateInterview(context *fiber.Ctx) error {
+	interview := new(model.Interview)
+
+	if err := context.BodyParser(interview); err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    "cannot parse JSON",
+		})
+	}
+
+	if err := controller.application.DB.Create(interview).Error; err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    err.Error(),
+		})
+	}
+
+	return context.JSON(fiber.Map{
+		"isSuccess": true,
+		"result":    interview,
+	})
+}
+
+func (controller *InterviewController) GetAllInterviews(context *fiber.Ctx) error {
+	var interviews []*model.Interview
+
+	if err := controller.application.DB.Preload("Instructor").
+		Preload("ApplicationReport").
+		Find(&interviews).Error; err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    err.Error(),
+		})
+	}
+
+	return context.JSON(fiber.Map{
+		"isSuccess": true,
+		"result":    interviews,
+	})
+}
+
+func (controller *InterviewController) GetInterviewByID(context *fiber.Ctx) error {
+	id := context.Params("id")
+	var interview model.Interview
+
+	if err := controller.application.DB.Preload("Instructor").
+		Preload("ApplicationReport").
+		First(&interview, id).Error; err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    "Interview not found",
+		})
+	}
+
+	return context.JSON(fiber.Map{
+		"isSuccess": true,
+		"result":    interview,
+	})
+}
+
+func (controller *InterviewController) UpdateInterview(context *fiber.Ctx) error {
+	id := context.Params("id")
+	var existingInterview model.Interview
+
+	if err := controller.application.DB.First(&existingInterview, id).Error; err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    "Interview not found",
+		})
+	}
+
+	var updateData model.Interview
+	if err := context.BodyParser(&updateData); err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    "cannot parse JSON",
+		})
+	}
+
+	if updateData.InstructorID != 0 {
+		existingInterview.InstructorID = updateData.InstructorID
+	}
+	if updateData.ApplicationReportID != 0 {
+		existingInterview.ApplicationReportID = updateData.ApplicationReportID
+	}
+	if !updateData.ScheduledAppointment.IsZero() {
+		existingInterview.ScheduledAppointment = updateData.ScheduledAppointment
+	}
+	if updateData.CriteriaScores != "" {
+		existingInterview.CriteriaScores = updateData.CriteriaScores
+	}
+	if updateData.TotalScore != 0 {
+		existingInterview.TotalScore = updateData.TotalScore
+	}
+	if !updateData.EvaluatedAt.IsZero() {
+		existingInterview.EvaluatedAt = updateData.EvaluatedAt
+	}
+	if updateData.InterviewStatus != "" {
+		existingInterview.InterviewStatus = updateData.InterviewStatus
+	}
+
+	if err := controller.application.DB.Save(&existingInterview).Error; err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    err.Error(),
+		})
+	}
+
+	return context.JSON(fiber.Map{
+		"isSuccess": true,
+		"result":    existingInterview,
+	})
+}
+
+func (controller *InterviewController) DeleteInterview(context *fiber.Ctx) error {
+	id := context.Params("id")
+
+	if err := controller.application.DB.Delete(&model.Interview{}, id).Error; err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    err.Error(),
+		})
+	}
+
+	return context.JSON(fiber.Map{
+		"isSuccess": true,
+		"result":    "Interview deleted successfully",
+	})
 }

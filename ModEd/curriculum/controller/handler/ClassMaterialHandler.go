3395ad5dca2fe6954a74d5923e58539a1f8d0026
wavekanit <wavekanit@hotmail.com>
2025-09-14 23:30:44 +0700

    feat: add create class material form

--- ModEd/curriculum/controller/handler/ClassMaterialHandler.go
+++ ModEd/curriculum/controller/handler/ClassMaterialHandler.go
@@ -1,22 +1,40 @@
 package handler
 
 import (
+	"ModEd/core"
 	"ModEd/curriculum/model"
+	"net/http"
+	"path/filepath"
 
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/gorm"
+	"github.com/hoisie/mustache"
 )
 
 type ClassMaterialHandler struct {
-	DB *gorm.DB
+	// DB          *gorm.DB
+	Application *core.ModEdApplication
 }
 
 func NewClassMaterialHandler() *ClassMaterialHandler {
 	return &ClassMaterialHandler{}
 }
 
-func (h *ClassMaterialHandler) RenderMain(context *fiber.Ctx) error {
-	return context.SendString("Class Material Main Page")
+// func (h *ClassMaterialHandler) RenderMain(context *fiber.Ctx) error {
+// 	return context.SendString("Class Material Main Page")
+// }
+
+func (h *ClassMaterialHandler) RenderCreateClassMaterial(c *fiber.Ctx) error {
+	path := filepath.Join(h.Application.RootPath, "curriculum", "view", "ClassMaterial.tpl")
+	tmpl, err := mustache.ParseFile(path)
+	if err != nil {
+		return c.SendStatus(http.StatusInternalServerError)
+	}
+	rendered := tmpl.Render(map[string]any{
+		"title":   "Create Class Material",
+		"RootURL": h.Application.RootURL,
+	})
+	c.Set("Content-Type", "text/html; charset=utf-8")
+	return c.SendString(rendered)
 }
 
 func (h *ClassMaterialHandler) CreateClassMaterial(context *fiber.Ctx) error {
@@ -25,7 +43,7 @@ func (h *ClassMaterialHandler) CreateClassMaterial(context *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
-	if err := h.DB.Create(payload).Error; err != nil {
+	if err := h.Application.DB.Create(payload).Error; err != nil {
 		return context.JSON(fiber.Map{
 			"isSuccess": false,
 			"result":    err.Error(),
@@ -48,7 +66,7 @@ func (h *ClassMaterialHandler) GetClassMaterialById(context *fiber.Ctx) error {
 	}
 
 	var classMaterial model.ClassMaterial
-	if err := h.DB.First(&classMaterial, id).Error; err != nil {
+	if err := h.Application.DB.First(&classMaterial, id).Error; err != nil {
 		return context.JSON(fiber.Map{
 			"isSuccess": false,
 			"result":    "failed to get curriculums",
@@ -63,7 +81,7 @@ func (h *ClassMaterialHandler) GetClassMaterialById(context *fiber.Ctx) error {
 
 func (h *ClassMaterialHandler) GetClassMaterials(context *fiber.Ctx) error {
 	var classMaterials []model.ClassMaterial
-	if err := h.DB.Find(&classMaterials).Error; err != nil {
+	if err := h.Application.DB.Find(&classMaterials).Error; err != nil {
 		return context.JSON(fiber.Map{
 			"isSuccess": false,
 			"result":    "failed to get class materials",
@@ -82,7 +100,7 @@ func (h *ClassMaterialHandler) UpdateClassMaterial(context *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
-	if err := h.DB.Save(payload).Error; err != nil {
+	if err := h.Application.DB.Save(payload).Error; err != nil {
 		return context.JSON(fiber.Map{
 			"isSuccess": false,
 			"result":    "failed to update class material",
@@ -104,7 +122,7 @@ func (h *ClassMaterialHandler) DeleteClassMaterial(context *fiber.Ctx) error {
 		})
 	}
 
-	if err := h.DB.Delete(&model.ClassMaterial{}, id).Error; err != nil {
+	if err := h.Application.DB.Delete(&model.ClassMaterial{}, id).Error; err != nil {
 		return context.JSON(fiber.Map{
 			"isSuccess": false,
 			"result":    "failed to delete class material",

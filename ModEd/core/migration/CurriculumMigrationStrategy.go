e56f1e199030946370a38e7c01f8b0bfc5f415a6
Minefreezey <wongsatorn.sung@hotmail.com>
2025-09-15 19:47:20 +0700

    chore: added dummy data for InternStudent

--- ModEd/core/migration/CurriculumMigrationStrategy.go
+++ ModEd/core/migration/CurriculumMigrationStrategy.go
@@ -7,34 +7,34 @@ type CurriculumMigrationStrategy struct {
 
 func (s *CurriculumMigrationStrategy) GetModels() []interface{} {
 	return []interface{}{
-		&model.Curriculum{},
-		&model.Course{},
-		&model.Class{},
-		&model.ClassMaterial{},
-		&model.CoursePlan{},
-		&model.Advisor{},
+		// &model.Curriculum{},
+		// &model.Course{},
+		// &model.Class{},
+		// &model.ClassMaterial{},
+		// &model.CoursePlan{},
+		// &model.Advisor{},
 		&model.Company{},
-		&model.InternshipReport{},
+		// &model.InternshipReport{},
 		// &model.InternshipResultEvaluation{},
-		&model.InternshipMentor{},
+		// &model.InternshipMentor{},
 		&model.InternStudent{},
-		&model.InternshipAttendance{},
-		&model.InternshipCriteria{},
+		// &model.InternshipAttendance{},
+		// &model.InternshipCriteria{},
 	}
 }
 
 func (s *CurriculumMigrationStrategy) GetSeedPath() []SeedPath {
 	return []SeedPath{
-		{Path: "data/common/DepartmentList.csv", Model: &model.Curriculum{}},
-		{Path: "data/common/DepartmentList.csv", Model: &model.Course{}},
-		{Path: "data/common/DepartmentList.csv", Model: &model.Class{}},
-		{Path: "data/common/DepartmentList.csv", Model: &model.ClassMaterial{}},
-		{Path: "data/common/DepartmentList.csv", Model: &model.CoursePlan{}},
-		{Path: "data/common/DepartmentList.csv", Model: &model.Advisor{}},
-		{Path: "data/common/DepartmentList.csv", Model: &model.Company{}},
-		{Path: "data/common/DepartmentList.csv", Model: &model.InternshipReport{}},
-		{Path: "data/common/DepartmentList.csv", Model: &model.InternshipMentor{}},
-		{Path: "data/common/DepartmentList.csv", Model: &model.InternStudent{}},
-		{Path: "data/common/DepartmentList.csv", Model: &model.InternshipAttendance{}},
+		// {Path: "data/common/DepartmentList.csv", Model: &model.Curriculum{}},
+		// {Path: "data/common/DepartmentList.csv", Model: &model.Course{}},
+		// {Path: "data/common/DepartmentList.csv", Model: &model.Class{}},
+		// {Path: "data/common/DepartmentList.csv", Model: &model.ClassMaterial{}},
+		// {Path: "data/common/DepartmentList.csv", Model: &model.CoursePlan{}},
+		// {Path: "data/common/DepartmentList.csv", Model: &model.Advisor{}},
+		{Path: "data/curriculum/Company.csv", Model: &[]model.Company{}},
+		// {Path: "data/common/DepartmentList.csv", Model: &model.InternshipReport{}},
+		// {Path: "data/common/DepartmentList.csv", Model: &model.InternshipMentor{}},
+		{Path: "data/curriculum/InternStudent.json", Model: &[]model.InternStudent{}},
+		// {Path: "data/common/DepartmentList.csv", Model: &model.InternshipAttendance{}},
 	}
 }

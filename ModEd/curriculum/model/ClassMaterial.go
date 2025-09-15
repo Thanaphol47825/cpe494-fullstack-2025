9c9f5eceab7fafbb6486f993557b81866f160879
wavekanit <wavekanit@hotmail.com>
2025-09-16 02:05:31 +0700

    fix: rename file name

--- ModEd/curriculum/model/ClassMaterial.go
+++ ModEd/curriculum/model/ClassMaterial.go
@@ -6,10 +6,10 @@ import (
 
 type ClassMaterial struct {
 	core.BaseModel
-	ClassId  uint   `gorm:"not null;" json:"class_id"`
+	ClassId  uint   `gorm:"not null;" csv:"class_id"`
 	Class    Class  `gorm:"foreignKey:ClassId;references:ID"`
-	FileName string `gorm:"type:varchar(100);not null" json:"file_name"`
-	FilePath string `gorm:"type:varchar(255);not null" json:"file_path"`
+	FileName string `gorm:"type:varchar(100);not null;" csv:"file_name"`
+	FilePath string `gorm:"type:varchar(255);not null;" csv:"file_path"`
 }
 
 func (ClassMaterial) TableName() string {

e391432830481734986fde2e922d3b3e2ad36965
Thanaphol(WD MBP) <thanaphol.iqtha@gmail.com>
2025-09-16 15:26:45 +0700

    feat: init core static

---
+++ ModEd/core/static/js/main.js
@@ -0,0 +1,12 @@
+// This is a main js file to render data that will be SPA application. write in OOP style.
+
+class ModEdApp {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        console.log("ModEd Application Initialized");
+        // Initialize your application here
+    }
+}

7dd746a6a44eb504d05e1af607485de9c86c8476
wavekanit <wavekanit@hotmail.com>
2025-09-21 21:25:06 +0700

    feat: Implement file Curriculum Application

--- ModEd/curriculum/static/js/CurriculumApplication.js
+++ ModEd/curriculum/static/js/CurriculumApplication.js
@@ -3,9 +3,22 @@ class CurriculumApplication {
     this.application = application
   }
 
+  model = ["Create Curriculum", "Create Class", "Create Class Material", "Create Student", "Create Instructor", "Test"]
+
   async render() {
-    console.log('Do something with CurriculumApplication.')
-    this.application.mainContainer.innerHTML = 'Overwritten with CurriculumApplication'
+    this.application.mainContainer.innerHTML = ""
+    
+    this.model.forEach((item) => {
+      let button = this.application.create(
+        `<a href="#" id="${item.replace(/\s+/g, '')}">
+          ${item}
+        </a>`,
+      )
+      button.onclick = () => console.log(eval(`new ${item.replace(/\s+/g, '')}(this).render()`))
+      
+      this.application.mainContainer.append(button)
+    })
+
     return false
   }
 }

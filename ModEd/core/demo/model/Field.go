50fe753f3c40fcb814e2d01b8a4e3aeb851ecce8
Arthit Nopjaroonsri <arthit.nop@gmail.com>
2025-10-06 16:13:52 +0700

    edit GenFormTable.md

--- ModEd/core/demo/model/Field.go
+++ ModEd/core/demo/model/Field.go
@@ -1,9 +1,9 @@
 package demo
 
 type Field struct {
-	Name    string `gorm:"type:varchar(255)" json:"Name" csv:"Name" form:"text" label:"Name"`
-	Surname string `gorm:"type:varchar(255)" json:"Surname" csv:"Surname" form:"text" label:"Surname"`
-	Age     int    `gorm:"type:int" json:"Age" csv:"Age" form:"number" label:"Age"`
+	Name    string `gorm:"type:varchar(255)" json:"name" csv:"Name" form:"text" label:"name"`
+	Surname string `gorm:"type:varchar(255)" json:"surname" csv:"Surname" form:"text" label:"surname"`
+	Age     int    `gorm:"type:int" json:"age" csv:"Age" form:"number" label:"age"`
 }
 
 type Admin struct {

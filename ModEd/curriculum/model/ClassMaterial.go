fbe2fc437f263f9d0a31e67e2a2d310e11ba279d
wavekanit <wavekanit@hotmail.com>
2025-10-13 23:56:13 +0700

    fix: select option in create form

--- ModEd/curriculum/model/ClassMaterial.go
+++ ModEd/curriculum/model/ClassMaterial.go
@@ -6,7 +6,7 @@ import (
 
 type ClassMaterial struct {
 	core.BaseModel
-	ClassId  uint   `gorm:"not null;" csv:"class_id" json:"ClassId" form:"label:Class;placeholder:Select Class;type:select;required:true;fk:Class;fklabel:Section"`
+	ClassId  uint   `gorm:"not null;" csv:"class_id" json:"ClassId" form:"label:Class;placeholder:Select Class;type:select;required:true;apiurl:/curriculum/Class/getClassOptions"`
 	Class    Class  `gorm:"foreignKey:ClassId;references:ID" form:"-"`
 	FileName string `gorm:"type:varchar(100);not null;" csv:"file_name" json:"FileName" form:"label:File Name;placeholder:Enter File Name;type:text;required:true"`
 	FilePath string `gorm:"type:varchar(255);not null;" csv:"file_path" json:"FilePath" form:"label:File Path;placeholder:Enter File Path;type:text;required:true"`

400b29eada129d079b4f89ba7d14e602e3fa32ee
wavekanit <wavekanit@hotmail.com>
2025-10-14 03:23:06 +0700

    hotfix: sort get all api

--- ModEd/curriculum/controller/handler/ClassHandler.go
+++ ModEd/curriculum/controller/handler/ClassHandler.go
@@ -70,7 +70,7 @@ func (h *ClassHandler) GetClassById(context *fiber.Ctx) error {
 
 func (h *ClassHandler) GetClasses(context *fiber.Ctx) error {
 	var classes []model.Class
-	if err := h.Application.DB.Preload("Course").Find(&classes).Error; err != nil {
+	if err := h.Application.DB.Preload("Course").Order("id ASC").Find(&classes).Error; err != nil {
 		return context.JSON(fiber.Map{
 			"isSuccess": false,
 			"result":    err.Error(),
@@ -124,22 +124,22 @@ func (h *ClassHandler) DeleteClass(context *fiber.Ctx) error {
 }
 
 func (h *ClassHandler) GetClassOptions(context *fiber.Ctx) error {
-    var classes []model.Class
-    var results []map[string]any
-    if err := h.Application.DB.Preload("Course").Find(&classes).Error; err != nil {
-        return context.JSON(fiber.Map{
-            "isSuccess": false,
-            "result":    "failed to get classes",
-        })
-    }
-    for _, class := range classes {
-        results = append(results, map[string]any{
-            "value": class.ID,
-            "label": class.Course.Name[:6] + " | " + class.Schedule.Format("2006-01-02 15:04"),
-        })
-    }
-    return context.JSON(fiber.Map{
-        "isSuccess": true,
-        "result":    results,
-    })
+	var classes []model.Class
+	var results []map[string]any
+	if err := h.Application.DB.Preload("Course").Order("id ASC").Find(&classes).Error; err != nil {
+		return context.JSON(fiber.Map{
+			"isSuccess": false,
+			"result":    "failed to get classes",
+		})
+	}
+	for _, class := range classes {
+		results = append(results, map[string]any{
+			"value": class.ID,
+			"label": class.Course.Name[:6] + " | " + class.Schedule.Format("2006-01-02 15:04"),
+		})
+	}
+	return context.JSON(fiber.Map{
+		"isSuccess": true,
+		"result":    results,
+	})
 }

513dcaf0d0d4eae28140a1df60b42e872befbfa9
aritazt <aritatgml@gmail.com>
2025-11-03 22:21:34 +0700

    remove console.log statements

--- ModEd/common/static/js/features/StudentList.js
+++ ModEd/common/static/js/features/StudentList.js
@@ -83,11 +83,9 @@ if (!window.CommonStudentListFeature) {
 
     async viewStudent(id) {
       try {
-        console.log("test1", id);
         const res = await fetch(`${this.rootURL}/common/students/${id}`);
         if (!res.ok) throw new Error(`HTTP ${res.status}`);
         const { result } = await res.json();
-        console.log("test2", result);
         alert(
           `Student Details\n` +
             `Name: ${result?.first_name ?? "-"} ${result?.last_name ?? ""}\n` +
